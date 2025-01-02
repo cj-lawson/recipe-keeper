9d12369ee5f26ba6ee8caca3bbe96d56973af8a7
CJ Lawson <clawson@MT06KW6PKW3.lan>
2025-01-02 17:10:13 -0600

    --

--- src/app/(app)/my-recipes/[userId]/page.tsx
+++ src/app/(app)/my-recipes/[userId]/page.tsx
@@ -10,7 +10,7 @@ export default async function MyRecipesPage({
 }: {
   params: { userId: string };
 }) {
-  const { userId } = params;
+  const userId = (await params).userId;
 
   // Ensure Supabase client is created
   const supabase = await createClient();
