809cad723bc82d7e31270cd7b6cc1751f10cae02
CJ Lawson <clawson@MT06KW6PKW3.lan>
2025-01-14 17:02:38 -0600

    sign up form validations

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -76,7 +76,7 @@ export default function signUp() {
                 autoComplete="current-password"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
-              <p className="text-red-500">{fields.password.errors}</p>
+              <p className="text-red-500 mt-2">{fields.password.errors}</p>
             </div>
           </div>
 
--- utils/zodSchemas.ts
+++ utils/zodSchemas.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const signUpSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(6).max(28),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters",
+    }).max(28, { message: "Passwords must be less than 28 characters" }),
 });
 
 export const logInSchema = z.object({
